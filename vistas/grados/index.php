2d4ffe8d61c43e2ac43e4a2fcd5db34c00422f8b
Livni <llmc0383@gmail.com>
2023-06-11 14:43:25 -0600

    se modifico el h1

--- vistas/grados/index.php
+++ vistas/grados/index.php
@@ -1,12 +1,12 @@
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
 <div class="container mt-5">
-  <h1 class="text-center mt-3">GRADOS MILITARES</h1>
+  <h1 class="text-center mt-3">Grado y Arma del Oficial</h1>
   <div class="row justify-content-center mt-2">
     <form action="/final_martinez/controladores/grados/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
       <div class="row mb-3">
         <div class="col">
-          <label for="grado" class="form-label">Ingrese un grado</label>
+          <label for="grado" class="form-label">Ingrese un grado y arma</label>
           <input type="text" name="gra_nombre" id="gra_nombre" class="form-control" required>
         </div>
       </div>
