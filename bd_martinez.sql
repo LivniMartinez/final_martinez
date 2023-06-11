774d65e1911bb6bd015381a39b497a7832f574f2
Livni <llmc0383@gmail.com>
2023-06-11 14:01:58 -0600

    se mejoro la base de datos con los not null correspondientes a cada tabla

--- bd_martinez.sql
+++ bd_martinez.sql
@@ -1,25 +1,25 @@
 create table programadores (
-    prog_id serial NOT NULL PRIMARY KEY,
-    prog_correo varchar(50),
-    prog_grado int, 
-    prog_nombreS varchar(50),
-    prog_apellidoS varchar(50),
-    prog_sit varchar(10),
+    prog_id serial not null primary key,
+    prog_correo varchar(50) not null,
+    prog_grado int not null, 
+    prog_nombreS varchar(50) not null,
+    prog_apellidoS varchar(50) not null,
+    prog_sit char (1) DEFAULT '1' ,
     FOREIGN KEY (prog_grado) REFERENCES grados(gra_id)
 );
 
 create table grados (
     gra_id serial not null,
-    gra_nombre varchar(30),
-    gra_sit varchar(10),
+    gra_nombre varchar(30) not null,
+    gra_sit char (1) DEFAULT '1',
     primary key (gra_id)
 );
 
 Create table asig_programador (
-asig_id serial primary key,
+asig_id serial not null primary key,
 asig_programador int not null,
 asig_app int not null,
-asig_sit int,
+asig_sit char (1) DEFAULT '1',
 foreign key (asig_programador) REFERENCES programadores (prog_id),
 foreign key (asig_app) REFERENCES aplicaciones (app_id)
 );
@@ -27,14 +27,14 @@ foreign key (asig_app) REFERENCES aplicaciones (app_id)
 Create table aplicaciones (
 app_id serial not null primary key,
 app_nombre varchar (50) not null,
-app_estado int
+app_estado char (1) DEFAULT '1'
 );
 
 Create table tareas (
 tar_id serial not null primary key,
-tar_app int,
+tar_app int not null,
 tar_descripcion varchar (50) not null,
-tar_fecha date,
-tar_estado int,
+tar_fecha date not null,
+tar_estado char (1) DEFAULT '1',
 foreign key (tar_app) REFERENCES aplicaciones(app_id)
 );
