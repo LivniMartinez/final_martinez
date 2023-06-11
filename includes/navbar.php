ab70ccc91aff543baca9236a9123f10a28ece89f
Livni <llmc0383@gmail.com>
2023-06-11 12:18:28 -0600

    se creo el navbar para la aplicacion

---
+++ includes/navbar.php
@@ -0,0 +1,96 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Aplicaciones</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        grados
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/ventas/index.php">Ingresar venta</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/ventas/buscar.php">Buscar venta</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/controladores/ventas/detalle_ventas.php">Detalle de venta</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Productos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/productos/index.php">Crear producto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/productos/buscar.php">Buscar producto</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Grados
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/grados/index.php">Crear Grado</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/grados/buscar.php">Buscar Grado</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Aplicaciones
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/aplicaciones/index.php">Guardar aplicacion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/aplicaciones/buscar.php">Buscar aplicacion</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    tareas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/tareas/index.php">Guardar Tarea</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/tareas/buscar.php">Buscar aplicacion</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Clientes
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/clientes/index.php">Crear cliente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/parcial_martinez/vistas/clientes/buscar.php">Buscar cliente</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
