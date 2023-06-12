38d0cbdc70a613ce6f488c7beb664f88f2bd7d05
Livni <llmc0383@gmail.com>
2023-06-11 20:56:13 -0600

    se modifico el codigo de programadores

--- modelos/Programadores.php
+++ modelos/Programadores.php
@@ -1,70 +1,107 @@
 <?php
 require_once 'Conexion.php';
 
-class Aplicacion extends Conexion{
+class Programadores extends Conexion {
     public $prog_id;
     public $prog_correo;
     public $prog_grado;
-    public $prog_nombreS;
-    public $prog_apellidoS;
+    public $prog_nombres;
+    public $prog_apellidos;
     public $prog_sit;
 
-
-    public function __construct($args = [] )
-    {
+    public function __construct($args = []) {
         $this->prog_id = $args['prog_id'] ?? null;
         $this->prog_correo = $args['prog_correo'] ?? '';
         $this->prog_grado = $args['prog_grado'] ?? '';
-        $this->prog_nombreS = $args['prog_nombreS'] ?? '';
-        $this->prog_apellidoS = $args['prog_apellidoS'] ?? '';
+        $this->prog_nombres = $args['prog_nombres'] ?? '';
+        $this->prog_apellidos = $args['prog_apellidos'] ?? '';
         $this->prog_sit = $args['prog_sit'] ?? 1;
     }
 
-    public function guardar(){
-        $sql = "INSERT INTO programadores(prog_correo,prog_grado, prog_nombreS, prog_apellidoS, prog_sit) VALUES ('$this->prog_correo','$this->prog_grado','$this->prog_nombreS''$this->prog_apellidoS','$this->prog_sit')";
+    public function guardar() {
+        $sql = "INSERT INTO programadores(prog_correo, prog_grado, prog_nombres, prog_apellidos, prog_sit) 
+                VALUES ('$this->prog_correo', '$this->prog_grado', '$this->prog_nombres', '$this->prog_apellidos', '$this->prog_sit')";
         $resultado = self::ejecutar($sql);
         return $resultado;
     }
 
-    public function buscar(){
-        $sql = "SELECT * FROM programadores WHERE prog_sit = 1";
+    public function buscar() {
+        $sql = "SELECT prog_id, prog_correo, gra_nombre || ' ' || prog_nombres || ' ' || prog_apellidos AS nombre
+        FROM programadores INNER JOIN grados on gra_id= prog_grado";
 
-        if($this->prog_correo != ''){
+        if ($this->prog_id != null) {
+            $sql .= " AND prog_id = $this->prog_id";
+        }
+
+        if ($this->prog_correo != '') {
             $sql .= " AND prog_correo LIKE '%$this->prog_correo%'";
         }
 
-        if($this->prog_grado != ''){
+        if ($this->prog_grado != '') {
             $sql .= " AND prog_grado LIKE '%$this->prog_grado%'";
         }
 
-        if($this->prog_nombreS != ''){
-            $sql .= " AND prog_nombreS LIKE '%$this->prog_nombreS%'";
+        if ($this->prog_nombres != '') {
+            $sql .= " AND prog_nombres LIKE '%$this->prog_nombres%'";
         }
 
-        if($this->prog_apellidoS != ''){
-            $sql .= " AND prog_apellidoS LIKE '%$this->prog_apellidoS%'";
+        if ($this->prog_apellidos != '') {
+            $sql .= " AND prog_apellidos LIKE '%$this->prog_apellidos%'";
         }
 
-        if($this->prog_sit != ''){
+        if ($this->prog_sit != '') {
             $sql .= " AND prog_sit = '$this->prog_sit'";
-        }
-
-        if($this->prog_id != null){
-            $sql .= " AND prog_id = $this->prog_id";
         }
 
         $resultado = self::servir($sql);
         return $resultado;
     }
 
-    public function modificar(){
-        $sql = "UPDATE programadores SET prog_correo = '$this->prog_correo', prog_grado = '$this->prog_grado', prog_nombreS = '$this->prog_nombreS', prog_apelllidoS = '$this->prog_apelllidoS', prog_sit = '$this->prog_sit' WHERE prog_id = $this->prog_id";
+
+    public function buscar2() {
+        $sql = "SELECT *
+        FROM programadores INNER JOIN grados on gra_id= prog_grado";
+
+        if ($this->prog_id != null) {
+            $sql .= " AND prog_id = $this->prog_id";
+        }
+
+        if ($this->prog_correo != '') {
+            $sql .= " AND prog_correo LIKE '%$this->prog_correo%'";
+        }
+
+        if ($this->prog_grado != '') {
+            $sql .= " AND prog_grado LIKE '%$this->prog_grado%'";
+        }
+
+        if ($this->prog_nombres != '') {
+            $sql .= " AND prog_nombres LIKE '%$this->prog_nombres%'";
+        }
+
+        if ($this->prog_apellidos != '') {
+            $sql .= " AND prog_apellidos LIKE '%$this->prog_apellidos%'";
+        }
+
+        if ($this->prog_sit != '') {
+            $sql .= " AND prog_sit = '$this->prog_sit'";
+        }
+
+        $resultado = self::servir($sql);
+        return $resultado;
+    }
+
+    public function modificar() {
+        $sql = "UPDATE programadores 
+                SET prog_correo = '$this->prog_correo', prog_grado = '$this->prog_grado', 
+                    prog_nombres = '$this->prog_nombres', prog_apellidos = '$this->prog_apellidos', 
+                    prog_sit = '$this->prog_sit' 
+                WHERE prog_id = $this->prog_id";
         
         $resultado = self::ejecutar($sql);
         return $resultado;
     }
 
-    public function eliminar(){
+    public function eliminar() {
         $sql = "DELETE FROM programadores WHERE prog_id = $this->prog_id";
         
         $resultado = self::ejecutar($sql);
