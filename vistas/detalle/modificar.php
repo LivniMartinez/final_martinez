32c57f8189225ddd74e41d524d7ae312f079dc62
Livni <llmc0383@gmail.com>
2023-06-12 16:33:50 -0600

    modificacion de estado

--- vistas/detalle/modificar.php
+++ vistas/detalle/modificar.php
@@ -20,7 +20,7 @@ try {
 
 <?php include_once '../../includes/header.php' ?>
 <div class="container">
-    <h1 class="text-center">Modificar Tareas</h1>
+    <h1 class="text-center">Modificar Estado</h1>
     <div class="row justify-content-center">
         <form action="/final_martinez/controladores/detalle/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
             <input type="hidden" name="tar_id" id="tar_id" value="<?= $Tareas[0]['TAR_ID'] ?>">
