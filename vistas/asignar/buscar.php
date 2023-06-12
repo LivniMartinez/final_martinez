e5bc2270a8fb51f66ffb8a3588b23125436e82b2
Livni <llmc0383@gmail.com>
2023-06-12 09:05:11 -0600

    modificacion  de el h1

--- vistas/asignar/buscar.php
+++ vistas/asignar/buscar.php
@@ -1,7 +1,7 @@
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Buscar tareas</h1>
+        <h1 class="text-center">Buscar tareas asignadas</h1>
         <div class="row justify-content-center">
             <form action="/final_martinez/controladores/asignar/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
