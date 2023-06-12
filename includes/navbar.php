100f274b48f4cbbb742f2f9dea6eb859f77b66b2
Livni <llmc0383@gmail.com>
2023-06-11 21:49:55 -0600

    Se modifico el Navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,31 +7,19 @@
             <a class="navbar-brand" href="#">Desarrollo de Apps_Martinez</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/final_martinez/vistas/login.php">Inicio</a>
+                    <a class="nav-link active" aria-current="page" href="/final_martinez/vistas/login.php"><i class="ion-ios-mic-off"></i></a>
                 </li>
+
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Grado y Arma
+                    Grado/Arma
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/grados/index.php">Crear Grado/Arma</a>
+                            <a class="nav-link" href="/final_martinez/vistas/grados/index.php">Crear Grado y arma</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/grados/buscar.php">Buscar Grado/Arma</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Programador
-                    </a>
-                    <ul class="dropdown-menu bg-dark">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/programadores/index.php">Crear Programador</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/programadores/buscar.php">Buscar Programador</a>
+                            <a class="nav-link" href="/final_martinez/vistas/grados/buscar.php">Buscar Grado y arma</a>
                         </li>
                     </ul>
                 </li>
@@ -41,23 +29,10 @@
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/aplicaciones/index.php">Crear aplicacion</a>
+                            <a class="nav-link" href="/final_martinez/vistas/aplicaciones/index.php">Guardar aplicacion</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/final_martinez/vistas/aplicaciones/buscar.php">Buscar aplicacion</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Asignación de APP
-                    </a>
-                    <ul class="dropdown-menu bg-dark">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/asig_app/index.php">Crear Asignación</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/asig_app/buscar.php">Buscar Asignación</a>
                         </li>
                     </ul>
                 </li>
@@ -67,12 +42,44 @@
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/tareas/index.php">Crear Tarea</a>
+                            <a class="nav-link" href="/final_martinez/vistas/tareas/index.php">Guardar Tarea</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/final_martinez/vistas/tareas/buscar.php">Buscar Tarea</a>
+                            <a class="nav-link" href="/final_martinez/vistas/tareas/buscar.php">Buscar aplicacion</a>
                         </li>
                     </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Programadores
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_martinez/vistas/programador/index.php">Registrar programador</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_martinez/vistas/programador/buscar.php">Buscar programador</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Asignar app
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_martinez/vistas/asignar/index.php"> asignar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_martinez/vistas/asignar/buscar.php">Buscar asignaciones</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " href="/final_martinez/vistas/detalle/index.php" >
+                        Detalle app
+                    </a>
+                   
                 </li>
             </ul>
         </div>
