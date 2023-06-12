cd5d4aa9c8829ccca5efa241581dec8f754b12dd
Livni <llmc0383@gmail.com>
2023-06-11 23:45:36 -0600

    modificacion navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -76,7 +76,7 @@
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
+                    <a class="nav-link  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
                         Detalle app
                     </a>
                     <ul class="dropdown-menu bg-dark">
