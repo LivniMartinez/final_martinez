8d577936c0112792625a64ad9246d8a5de0a415f
Livni <llmc0383@gmail.com>
2023-06-11 23:10:16 -0600

    se modifico codigo

--- vistas/grados/modificar.php
+++ vistas/grados/modificar.php
@@ -14,13 +14,13 @@ require '../../modelos/Grados.php';
 ?>
 <?php include_once '../../includes/header.php'?>
     <div class="container">
-        <h1 class="text-center">Modificar grados</h1>
+        <h1 class="text-center">Modificar grado y arma</h1>
         <div class="row justify-content-center">
             <form action="/final_martinez/controladores/grados/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <input type="hidden" name="gra_id" value="<?= $grados[0]['GRA_ID'] ?>" >
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="gra_nombre">Nombre del cliente</label>
+                        <label for="gra_nombre">Grado y Arma</label>
                         <input type="text" name="gra_nombre" id="gra_nombre" value="<?= $grados[0]['GRA_NOMBRE'] ?>" class="form-control" required>
                     </div>
                 </div>
